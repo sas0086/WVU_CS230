e8fe82678a06fea9a7d607521c3bc36a35519b9b
sas0086 <sas0086@mix.wvu.edu>
2021-09-09 16:42:45 -0400

    enter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
 import { TopNavBar2Component } from './header/top-nav-bar-2.component'
 import { FilterComponent } from './header/filter.component';
+import { Card1Component } from './left-menu/card-1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
     TopNavBar2Component,
-    FilterComponent
+    FilterComponent,
+    Card1Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/left-menu/card-1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-card-1',
+    templateUrl: 'card-1.component.html',
+    styleUrls: ['./card-1.component.css']
+})
+export class Card1Component{
+
+}

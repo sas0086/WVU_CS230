84fccad91e2098667f7b5dabe62f636842d0effe
sas0086 <sas0086@mix.wvu.edu>
2021-09-09 04:22:01 -0400

    enter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
-import { TopNavBar2Component } from './header/top-nav-bar-2.component';
+import { TopNavBar2Component } from './header/top-nav-bar-2.component'
+import { FilterComponent } from './header/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    TopNavBar2Component
+    TopNavBar2Component,
+    FilterComponent
   ],
   imports: [
     BrowserModule

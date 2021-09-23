853b509e47d988ce8b917dd92861e4ec971f3858
sas0086 <sas0086@mix.wvu.edu>
2021-09-22 21:30:13 -0400

    Web page components added w/o layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { TopNavBar2Component } from './header/top-nav-bar-2.component'
 import { FilterComponent } from './header/filter.component';
 import { Card1Component } from './left-menu/card-1.component';
 import { OverallLayoutComponent } from './layout/overall-layout.component';
+import { EmployersPageComponent } from './web-page/employers-page.component';
+import { EventsPageComponent } from './web-page/events-page.component';
+import { HomePageComponent } from './web-page/home-page.component';
+import { MessagesPageComponent } from './web-page/messages-page.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +19,11 @@ import { OverallLayoutComponent } from './layout/overall-layout.component';
     TopNavBar2Component,
     FilterComponent,
     Card1Component,
-    OverallLayoutComponent
+    OverallLayoutComponent,
+    EmployersPageComponent,
+    EventsPageComponent,
+    HomePageComponent,
+    MessagesPageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/web-page/employers-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-employers-page',
+    templateUrl: 'employers-page.component.html',
+    styleUrls: ['./employers-page.component.css']
+})
+export class EmployersPageComponent{
+
+}

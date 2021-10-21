1b2254de97e1b926b994482ed705c9a485d5451f
sas0086 <sas0086@mix.wvu.edu>
2021-10-20 23:01:31 -0400

    added user info service

--- src/app/header/user-info.component.ts
+++ src/app/header/user-info.component.ts
@@ -1,30 +1,23 @@
-import { HttpClient } from "@angular/common/http";
 import { Component, Injectable, OnInit } from "@angular/core";
 import { UserInfo } from "./user-info.model";
+import { UserInfoService } from "./user-info.service";
 
-@Injectable()
 @Component({
     selector:"handshake-user-info",
     templateUrl:"user-info.component.html"
 })
-
 export class UserInfoComponent implements OnInit{
     myInfo: UserInfo | undefined;
 
-    constructor(private http:HttpClient){
+    constructor(private userInfoService:UserInfoService){
     }
     ngOnInit(): void {
-        console.log("sending a get request to the server");
-        this.getUserInfo();
         console.log("registering showUserInfo as a subscriber");
         this.showUserInfo();
     }
 
-    getUserInfo(){
-        return this.http.get<UserInfo>('https://handshake-app-49bb0-default-rtdb.firebaseio.com/my-info.json');
-    }
     showUserInfo(){
-        this.getUserInfo().subscribe((data: UserInfo) => {
+        this.userInfoService.getUserInfo().subscribe((data: UserInfo) => {
             console.log(data);
             this.myInfo=data;
         })
---
+++ src/app/header/user-info.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { UserInfo } from "./user-info.model";
+
+@Injectable({providedIn: 'root'})
+export class UserInfoService{
+    private baseUrl:string = 'https://handshake-app-49bb0-default-rtdb.firebaseio.com/';
+    private myInfoEndpoint:string = 'my-info.json';
+    constructor(private http:HttpClient){
+        
+    }
+
+    getUserInfo(){
+        return this.http.get<UserInfo>(this.baseUrl + this.myInfoEndpoint);
+    }
+}
